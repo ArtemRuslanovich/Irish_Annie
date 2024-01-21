579e109ce4631a7fa3510f45d76a85f970879992
Admin <artem-ruslanovich@inbox.ru>
2024-01-22 00:05:18 +0300

    rebuild database conn

--- utils/postgresdata.py
+++ utils/postgresdata.py
@@ -1,7 +1,7 @@
 import asyncpg
 
 
-DATABASE_URL = "postgresql://postgres:80156120189fap@localhost/Ai_bot"
+DATABASE_URL = "postgres://uutzeepoeiudrz:b1f9e4066b489e6aa6d432634ce5f9df1305fb54a88c6a2356925b70773d5cf4@ec2-54-234-13-16.compute-1.amazonaws.com:5432/da7qujukih0gqg"
 
 
 async def connect_to_db():
@@ -11,8 +11,8 @@ async def close_db_connection(connection):
     await connection.close()
 
 async def create_pool():
-    return await asyncpg.create_pool(user='postgres',
-                                    password='80156120189fap',
-                                    database='Ai_bot', host='localhost',
+    return await asyncpg.create_pool(user='uutzeepoeiudrz',
+                                    password='b1f9e4066b489e6aa6d432634ce5f9df1305fb54a88c6a2356925b70773d5cf4',
+                                    database='da7qujukih0gqg', host='ec2-54-234-13-16.compute-1.amazonaws.com',
                                     port='5432',
                                     command_timeout=60)
