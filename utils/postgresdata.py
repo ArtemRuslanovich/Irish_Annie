984204b80f0b99ac77cbdb386b2f4b9f91ec894e
Admin <artem-ruslanovich@inbox.ru>
2024-01-22 22:33:13 +0300

    rebuild postgres conn

--- utils/postgresdata.py
+++ utils/postgresdata.py
@@ -1,7 +1,7 @@
 import asyncpg
 
 
-DATABASE_URL = "postgres://gufsckxwaehtpv:86d93b93e6526ff7dc4bcc2a40cb6d1798f32811ffd5ceb6208fc011c8181e7d@ec2-54-234-13-16.compute-1.amazonaws.com:5432/dbjia233jsi6m2"
+DATABASE_URL = "postgres://fylesvsuxnwlzy:d7c6eae8dc7d78689045cb4e59917d033f10af7992bf2b7851fb9582ba892ff6@ec2-34-238-201-192.compute-1.amazonaws.com:5432/dedbfa2fsjhcua"
 
 
 async def connect_to_db():
@@ -11,8 +11,4 @@ async def close_db_connection(connection):
     await connection.close()
 
 async def create_pool():
-    return await asyncpg.create_pool(user='gufsckxwaehtpv',
-                                    password='86d93b93e6526ff7dc4bcc2a40cb6d1798f32811ffd5ceb6208fc011c8181e7d',
-                                    database='dbjia233jsi6m2', host='ec2-54-234-13-16.compute-1.amazonaws.com',
-                                    port='5432',
-                                    command_timeout=60, ssl='require')
+    return await asyncpg.create_pool(DATABASE_URL, ssl='require')
