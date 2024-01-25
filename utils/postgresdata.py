1845b128e7719992aff857cc06bee5a9202615e7
Admin <artem-ruslanovich@inbox.ru>
2024-01-25 22:08:35 +0300

    d

--- utils/postgresdata.py
+++ utils/postgresdata.py
@@ -1,7 +1,7 @@
 import asyncpg
 
 
-DATABASE_URL = "postgres://fylesvsuxnwlzy:d7c6eae8dc7d78689045cb4e59917d033f10af7992bf2b7851fb9582ba892ff6@ec2-34-238-201-192.compute-1.amazonaws.com:5432/dedbfa2fsjhcua "
+DATABASE_URL = "postgres://fylesvsuxnwlzy:d7c6eae8dc7d78689045cb4e59917d033f10af7992bf2b7851fb9582ba892ff@ec2-34-238-201-192.compute-1.amazonaws.com:5432/dedbfa2fsjhcua"
 
 async def connect_to_db():
     return await asyncpg.connect(DATABASE_URL, ssl='require')
