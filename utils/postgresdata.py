87deab69c2235ef0e19e44ba68d07fe442acdb42
Admin <artem-ruslanovich@inbox.ru>
2024-01-25 21:28:48 +0300

    f

--- utils/postgresdata.py
+++ utils/postgresdata.py
@@ -3,12 +3,11 @@ import asyncpg
 
 DATABASE_URL = "postgres://fylesvsuxnwlzy:d7c6eae8dc7d78689045cb4e59917d033f10af7992bf2b7851fb9582ba892ff6@ec2-34-238-201-192.compute-1.amazonaws.com:5432/dedbfa2fsjhcua"
 
-
 async def connect_to_db():
-    return await asyncpg.connect(DATABASE_URL, ssl='require')
+    return await asyncpg.connect(DATABASE_URL)
 
 async def close_db_connection(connection):
     await connection.close()
-#g
+
 async def create_pool():
-    return await asyncpg.create_pool(DATABASE_URL, ssl='require')
+    return await asyncpg.create_pool(DATABASE_URL)
