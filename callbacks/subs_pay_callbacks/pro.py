8da50efd1db3d354dd1bdc6930f0058539201860
Admin <artem-ruslanovich@inbox.ru>
2024-02-04 13:42:37 +0300

    add sub plans

--- callbacks/subs_pay_callbacks/pro.py
+++ callbacks/subs_pay_callbacks/pro.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message
 
-STRIPE_PAYMENT_LINK = 'https://buy.stripe.com/5kA8yL9NC4US4UM144'
+STRIPE_PAYMENT_LINK = '*'
 
 
 async def process_subscribe_command(message: Message):
