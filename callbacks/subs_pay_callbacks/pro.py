809ff885969b512a93a7f416ee1ebe828e048c2f
Admin <artem-ruslanovich@inbox.ru>
2024-02-04 22:15:13 +0300

    ref code

--- callbacks/subs_pay_callbacks/pro.py
+++ callbacks/subs_pay_callbacks/pro.py
@@ -1,8 +1,9 @@
 from aiogram.types import Message
+from aiogram import Bot
 
 STRIPE_PAYMENT_LINK = '*'
 
 
-async def process_subscribe_pro(message: Message):
+async def process_subscribe_pro(bot: Bot, message: Message):
     # Send a message with the Stripe Payment Link
-    await message.answer("You can subscribe by following this link: " + STRIPE_PAYMENT_LINK)
+    await bot.send_message(chat_id=message.chat.id, text=f"You can subscribe by following this link: + {STRIPE_PAYMENT_LINK}")
