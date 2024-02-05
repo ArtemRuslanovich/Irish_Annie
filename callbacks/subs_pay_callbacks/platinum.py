185842e950b9b6fde10c2e9809d347ff49610a96
Admin <artem-ruslanovich@inbox.ru>
2024-02-05 17:48:55 +0300

    test

--- callbacks/subs_pay_callbacks/platinum.py
+++ callbacks/subs_pay_callbacks/platinum.py
@@ -1,9 +1,15 @@
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import CallbackQuery
 from aiogram import Bot
 
-STRIPE_PAYMENT_LINK = '*'
+STRIPE_PAYMENT_LINK = 'https://buy.stripe.com/test_123'
 
-
-async def process_subscribe_platinum(callback_query: CallbackQuery, message: Message):
-    # Send a message with the Stripe Payment Link
-    await callback_query.message.edit_text(f"You can subscribe by following this link: {STRIPE_PAYMENT_LINK}")
+async def process_subscribe_platinum(callback_query: CallbackQuery):
+    # Attempt to edit the message to include the Stripe Payment Link
+    new_text = f"You can subscribe by following this link: {STRIPE_PAYMENT_LINK}"
+    
+    # Check if the message is already what you're trying to set it to
+    if callback_query.message.text != new_text:
+        await callback_query.message.edit_text(new_text)
+    else:
+        # If the message is already displaying the link, consider acknowledging the action without editing
+        await callback_query.answer("You're already on the subscription page!", show_alert=True)
