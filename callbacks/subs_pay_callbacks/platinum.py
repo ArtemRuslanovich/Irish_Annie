0d803d0366fcc67287fa204d046047b9201ad708
Admin <artem-ruslanovich@inbox.ru>
2024-02-05 15:46:32 +0300

    new changes

--- callbacks/subs_pay_callbacks/platinum.py
+++ callbacks/subs_pay_callbacks/platinum.py
@@ -6,4 +6,4 @@ STRIPE_PAYMENT_LINK = '*'
 
 async def process_subscribe_platinum(bot: Bot, message: Message):
     # Send a message with the Stripe Payment Link
-    await bot.send_message(chat_id=message.chat.id, text=f"You can subscribe by following this link: + {STRIPE_PAYMENT_LINK}")
+    await bot.send_message(chat_id=message.chat.id, text=f"You can subscribe by following this link: {STRIPE_PAYMENT_LINK}")
