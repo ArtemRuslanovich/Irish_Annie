fd1711a01fe5d659908bf1fcbbf43595997ca2ee
Admin <artem-ruslanovich@inbox.ru>
2024-02-08 14:33:24 +0300

    add dynamic keyboard

--- callbacks/credits.py
+++ callbacks/credits.py
@@ -1,13 +1,15 @@
 from aiogram.types import CallbackQuery
 from aiogram import Router, F, Bot, Dispatcher
-from keyboards.credits import credits_keyboard
+from keyboards.credits import get_keyboard
 from aiogram.fsm.context import FSMContext
 from utils.statesform import StatesForm
 
 
 async def credits(callback: CallbackQuery, state: FSMContext):
     await state.set_state(StatesForm.get_credits)
+    user_id = callback.from_user.id
+    keyboard = get_keyboard(user_id)
     await callback.message.edit_text(
         f"💳 More credits, more fun with Annie. Choose now. 👄👅",
-        reply_markup=credits_keyboard
+        reply_markup=keyboard
     )
--- keyboards/credits.py
+++ keyboards/credits.py
@@ -2,60 +2,60 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import CallbackQuery
 
 
-user_id = CallbackQuery.from_user.id
-credits_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(
-            text='100 →  $1.49',
-            callback_data='credits_100'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='200 →  $2.99',
-            callback_data='credits_200'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='500 →  $4.99',
-            callback_data='credits_500'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='1000 →  $8.99',
-            callback_data='credits_1000'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='2500 →  $17.99',
-            callback_data='credits_2500'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='5000 →  $29.99',
-            callback_data='credits_5000'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='10000 →  $49.99',
-            callback_data='credits_10000'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text=f'❤️Subscribe monthly for 50% discount and 20000 credits❤️',
-            url=f'https://probably-kit.github.io/subscription-choice-menu/?user_id={user_id}'
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='◀️ Back',
-            callback_data='profile'
-        )
-    ]
-])
+async def get_keyboard(user_id):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='100 →  $1.49',
+                callback_data='credits_100'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='200 →  $2.99',
+                callback_data='credits_200'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='500 →  $4.99',
+                callback_data='credits_500'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='1000 →  $8.99',
+                callback_data='credits_1000'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='2500 →  $17.99',
+                callback_data='credits_2500'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='5000 →  $29.99',
+                callback_data='credits_5000'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='10000 →  $49.99',
+                callback_data='credits_10000'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=f'❤️Subscribe monthly for 50% discount and 20000 credits❤️',
+                url=f'https://probably-kit.github.io/subscription-choice-menu/?user_id={user_id}'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='◀️ Back',
+                callback_data='profile'
+            )
+        ]
+    ])
